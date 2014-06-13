698a245e374b6f5493695d15c714066720b99258
Sam Carecho <digiwise@gmail.com>
2014-06-13 16:13:20 -0300

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name anatelFrontApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the anatelFrontApp
+ */
+angular.module('anatelFrontApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
